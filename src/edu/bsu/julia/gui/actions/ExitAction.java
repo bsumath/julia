0a8f52f4a7c3c60e81b8c4ed663ad784e8438ee2
Ben Dean <benthedean@gmail.com>
2008-05-23 14:36:19 +0000

    changed the ExitAction to make use of the save/discard/cancel dialog
    rebuilt jars

--- src/edu/bsu/julia/gui/actions/ExitAction.java
+++ src/edu/bsu/julia/gui/actions/ExitAction.java
@@ -1,33 +1,39 @@
 package edu.bsu.julia.gui.actions;
 
 import java.awt.event.ActionEvent;
+
 import javax.swing.AbstractAction;
 import javax.swing.ImageIcon;
-import javax.swing.JOptionPane;
+
 import edu.bsu.julia.Julia;
+import edu.bsu.julia.gui.SaveSessionDialog;
 
 public class ExitAction extends AbstractAction {
-	
+
 	private Julia parentFrame;
-	//for serializable interface: do not use
+	// for serializable interface: do not use
 	public static final long serialVersionUID = 0;
-	
+
 	public ExitAction(Julia f) {
-		super("Exit", new ImageIcon
-				(Thread.currentThread().getContextClassLoader().getResource
-						("exit.png")));
+		super("Exit", new ImageIcon(Thread.currentThread()
+				.getContextClassLoader().getResource("exit.png")));
 		putValue("SHORT_DESCRIPTION", "Exit Program");
-		putValue("LONG_DESCRIPTION", "Exit the program.  Unsaved " +
-				"data will be lost.");
+		putValue("LONG_DESCRIPTION", "Exit the program.  Unsaved "
+				+ "data will be lost.");
 		parentFrame = f;
 	}
 
 	public void actionPerformed(ActionEvent arg0) {
-		int choice;
-		choice = JOptionPane.showConfirmDialog(parentFrame,
-				"Are you sure?\nUnsaved data will be lost.",
-				"Exit?",JOptionPane.OK_CANCEL_OPTION);
-		if (choice == JOptionPane.OK_OPTION) System.exit(0);
+		parentFrame.loseFocus();
+
+		// show the save session dialog if necessary
+		if (parentFrame.getCurrentSession().isModified()) {
+			SaveSessionDialog saveDialog = new SaveSessionDialog(parentFrame);
+			if (saveDialog.showSaveDialog() == SaveSessionDialog.CANCELED)
+				return;
+		}
+
+		System.exit(0);
 	}
 
 }
