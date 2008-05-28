833f945c3f30d071197a5729e8651f25d78f60c9
benthedean <benthedean@localhost>
2008-05-28 04:15:22 +0000

    fixed bug with pop-up menus for input and output showing up even when input and/or output are empty.
    rebuilt jars

--- src/edu/bsu/julia/gui/actions/ChangeColorAction.java
+++ src/edu/bsu/julia/gui/actions/ChangeColorAction.java
@@ -1,39 +1,42 @@
 package edu.bsu.julia.gui.actions;
 
-import java.awt.event.*;
-import edu.bsu.julia.*;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
 
-import javax.swing.*;
-
-import java.awt.*;
+import javax.swing.AbstractAction;
 import javax.swing.DefaultListModel;
+import javax.swing.ImageIcon;
+import javax.swing.JColorChooser;
+
+import edu.bsu.julia.Julia;
+import edu.bsu.julia.OutputFunction;
 
 public class ChangeColorAction extends AbstractAction {
-	
+
 	private Julia parentFrame;
 	private DefaultListModel outputList;
 	private int index;
-	//for serializable interface: do not use
+	// for serializable interface: do not use
 	public static final long serialVersionUID = 0;
-	
+
 	public ChangeColorAction(Julia f, DefaultListModel listModel, int i) {
-		super("Change Color", new ImageIcon
-				(Thread.currentThread().getContextClassLoader().getResource
-						("color.png")));
+		super("Change Color", new ImageIcon(Thread.currentThread()
+				.getContextClassLoader().getResource("color.png")));
 		putValue("SHORT_DESCRIPTION", "Change Color");
-		putValue("LONG_DESCRIPTION", "Change the color used to display " +
-				"this function.");
+		putValue("LONG_DESCRIPTION", "Change the color used to display "
+				+ "this function.");
 		parentFrame = f;
 		outputList = listModel;
 		index = i;
 	}
 
 	public void actionPerformed(ActionEvent arg0) {
-		OutputFunction fn = (OutputFunction)outputList.getElementAt(index);
-		
-		Color newColor = JColorChooser.showDialog(parentFrame, 
+		OutputFunction fn = (OutputFunction) outputList.getElementAt(index);
+
+		Color newColor = JColorChooser.showDialog(parentFrame,
 				"New Color for Function Display", fn.getColor());
-		if(newColor!=null) fn.setColor(newColor);
+		if (newColor != null)
+			fn.setColor(newColor);
 	}
 
 }
