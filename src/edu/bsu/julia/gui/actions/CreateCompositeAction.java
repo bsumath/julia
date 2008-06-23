e0f45f510c946128d8b26dbe8a998cb0512c4552
Ben Dean <benthedean@gmail.com>
2008-06-23 18:03:05 +0000

    changed CreateCompositeAction to use the generators

--- src/edu/bsu/julia/gui/actions/CreateCompositeAction.java
+++ src/edu/bsu/julia/gui/actions/CreateCompositeAction.java
@@ -2,177 +2,106 @@ package edu.bsu.julia.gui.actions;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.AbstractAction;
 import javax.swing.ButtonGroup;
 import javax.swing.ProgressMonitor;
 import javax.swing.Timer;
 
+import edu.bsu.julia.ComplexNumber;
 import edu.bsu.julia.Julia;
+import edu.bsu.julia.generators.ErgodicAttrOutputSetGenerator;
+import edu.bsu.julia.generators.ErgodicJuliaOutputSetGenerator;
+import edu.bsu.julia.generators.FullAttrOutputSetGenerator;
+import edu.bsu.julia.generators.FullJuliaOutputSetGenerator;
+import edu.bsu.julia.generators.OutputSetGenerator;
+import edu.bsu.julia.generators.FullAttrOutputSetGenerator.Options;
 import edu.bsu.julia.gui.InputPanel;
-import edu.bsu.julia.threads.CErgodicAttrThread;
-import edu.bsu.julia.threads.CErgodicJuliaThread;
-import edu.bsu.julia.threads.CFullAttrThread;
-import edu.bsu.julia.threads.CFullJuliaThread;
+import edu.bsu.julia.input.InputFunction;
+import edu.bsu.julia.session.Session;
 
+/**
+ * an {@link AbstractAction} to create a composite output set
+ * @author Ben Dean
+ */
 public class CreateCompositeAction extends AbstractAction {
-	
-	private Julia parentFrame;
-	private ButtonGroup typeGroup;
-	private ButtonGroup methodGroup;
-	private InputPanel inputPanel;
-	private ProgressMonitor pm;
-	private CErgodicJuliaThread ejThread;
-	private CFullJuliaThread fjThread;
-	private CErgodicAttrThread eaThread;
-	private CFullAttrThread faThread;
-	//for serializable interface: do not use
+	private final Julia parentFrame;
+	private final ButtonGroup typeGroup;
+	private final ButtonGroup methodGroup;
+	private final ProgressMonitor pm;
+	private Thread thread;
+	private OutputSetGenerator generator;
+
+	// for serializable interface: do not use
 	public static final long serialVersionUID = 0;
-	
+
 	public CreateCompositeAction(Julia f, ButtonGroup t, ButtonGroup m) {
 		super("Composite");
 		parentFrame = f;
 		typeGroup = t;
 		methodGroup = m;
 		putValue("SHORT_DESCRIPTION", "Create Composite Set");
-		putValue("LONG_DESCRIPTION", "Create a Composite Set " +
-				"from the selected functions.");
+		putValue("LONG_DESCRIPTION", "Create a Composite Set "
+				+ "from the selected functions.");
+		
+		// create the progress monitor
+		pm = new ProgressMonitor(parentFrame, "Processing Functions...", "", 0,
+				100);
 	}
 
-	public void actionPerformed(ActionEvent arg0) 
-	{
+	public void actionPerformed(ActionEvent arg0) {
 		parentFrame.getOutputFunctionList().clearSelection();
-		inputPanel = parentFrame.getInputPanel();
-		if (typeGroup.getSelection().getActionCommand() == "julia") 
-		{
-			if(methodGroup.getSelection().getActionCommand() == "ergodic")
-				ergodicJulia();
-			else fullJulia();
-		}
-		else 
-		{
-			if(methodGroup.getSelection().getActionCommand() == "ergodic") 
-				ergodicAttr();
-			else fullAttr();
-		}
-	}
-	
-	private void ergodicJulia() 
-	{
-		int noOfFns = inputPanel.getSelectedFunctions().length;
-		int iterations = parentFrame.getCurrentSession().getIterations();
-		pm = new ProgressMonitor(parentFrame,
-			"Processing Functions...",
-			"", 0, (iterations)*(noOfFns+1));
-		ejThread = new CErgodicJuliaThread(parentFrame);
-		ejThread.start();
-		Timer timer = new Timer(500, new TimerActionListener(0));
-		timer.start();
-	}
-	
-	private void fullJulia() 
-	{
-		int noOfFns = inputPanel.getSelectedFunctions().length;
-		int iterations = parentFrame.getCurrentSession().getIterations();
-		pm = new ProgressMonitor(parentFrame,
-			"Processing Functions...",
-			"", 0, (iterations)*(noOfFns+1));
-		fjThread = new CFullJuliaThread(parentFrame);
-		fjThread.start();
-		Timer timer = new Timer(500, new TimerActionListener(1));
-		timer.start();
-	}
-	
-	private void ergodicAttr() 
-	{
-		int noOfFns = inputPanel.getSelectedFunctions().length;
-		int iterations = parentFrame.getCurrentSession().getIterations();
-		pm = new ProgressMonitor(parentFrame,
-			"Processing Functions...",
-			"", 0, (iterations)*(noOfFns+1));
-		eaThread = new CErgodicAttrThread(parentFrame);
-		eaThread.start();
-		Timer timer = new Timer(500, new TimerActionListener(2));
-		timer.start();
-	}
-	
-	private void fullAttr() 
-	{
-		int noOfFns = inputPanel.getSelectedFunctions().length;
-		int iterations = parentFrame.getCurrentSession().getIterations();
-		pm = new ProgressMonitor(parentFrame,
-			"Processing Functions...",
-			"", 0, (iterations)*(noOfFns+1));
-		faThread = new CFullAttrThread(parentFrame);
-		faThread.start();
-		Timer timer = new Timer(500, new TimerActionListener(3));
-		timer.start();
-	}
-	
-	private class TimerActionListener implements ActionListener 
-	{
-		
-		int threadType;
-		public static final int EJTHREAD = 0;
-		public static final int FJTHREAD = 1;
-		public static final int EATHREAD = 2;
-		public static final int FATHREAD = 3;
-		
-		public TimerActionListener(int type) 
-		{
-			threadType = type;
+		Session session = parentFrame.getCurrentSession();
+		InputPanel inputPanel = parentFrame.getInputPanel();
+
+		// build list of input functions
+		List<InputFunction> inFunc = new ArrayList<InputFunction>();
+		for (InputFunction func : inputPanel.getSelectedFunctions()) {
+			inFunc.add(func);
 		}
 
-		public void actionPerformed(ActionEvent e) 
-		{
-			switch(threadType) 
-			{
-				case 0:
-					if(!pm.isCanceled()&&ejThread.isAlive()) 
-					{
-						pm.setProgress(ejThread.getProgress());
-					}
-					else 
-					{
-						ejThread.setStop();
-						pm.close();
-					}
-					break;
-				case 1:
-					if(!pm.isCanceled()&&fjThread.isAlive()) 
-					{
-						pm.setProgress(fjThread.getProgress());
-					}
-					else 
-					{
-						fjThread.setStop();
-						pm.close();
-					}
-					break;
-				case 2:
-					if(!pm.isCanceled()&&eaThread.isAlive()) 
-					{
-						pm.setProgress(eaThread.getProgress());
-					}
-					else 
-					{
-						eaThread.setStop();
-						pm.close();
-					}
-					break;
-				case 3:
-					if(!pm.isCanceled()&&faThread.isAlive()) 
-					{
-						pm.setProgress(faThread.getProgress());
-					}
-					else 
-					{
-						faThread.setStop();
-						pm.close();
-					}
+		// create a command string based on which options are selected
+		String command = typeGroup.getSelection().getActionCommand() + "+"
+				+ methodGroup.getSelection().getActionCommand();
+
+		// create a generator for the command selected
+		if (command.equals("julia+ergodic")) {
+			generator = new ErgodicJuliaOutputSetGenerator(parentFrame, session
+					.getIterations(), session.getSkips(), session
+					.getSeedValue(), inFunc);
+		} else if (command.equals("julia+full")) {
+			generator = new FullJuliaOutputSetGenerator(parentFrame, session
+					.getIterations(), session.getSeedValue(), inFunc);
+		} else if (command.equals("attr+ergodic")) {
+			generator = new ErgodicAttrOutputSetGenerator(parentFrame, session
+					.getIterations(), session.getSkips(), session
+					.getSeedValue(), inFunc);
+		} else if (command.equals("attr+full")) {
+			List<ComplexNumber> seedList = new ArrayList<ComplexNumber>();
+			seedList.add(session.getSeedValue());
+			generator = new FullAttrOutputSetGenerator(parentFrame, session
+					.getIterations(), seedList, inFunc,
+					Options.DISCARD_INTERMEDIATE_POINTS);
+		}
+
+		// start the thread and timer
+		new Thread(generator).start();
+		new Timer(500, new TimerActionListener()).start();
+	}
+
+	private final class TimerActionListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			if (!pm.isCanceled() && thread.isAlive()) {
+				int progress = (int) (generator.getPercentComplete() * 100);
+				progress = (progress < 0) ? 0 : progress;
+				progress = (progress > 100) ? 100 : progress;
+				pm.setProgress(progress);
+			} else {
+				generator.cancelExecution();
+				pm.close();
 			}
 		}
-		
 	}
-
 }
