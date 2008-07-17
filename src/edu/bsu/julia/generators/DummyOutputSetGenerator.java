096945a97a44bfe0dda1792c58aa3ce15af06d78
Ben Dean <benthedean@gmail.com>
2008-07-17 14:31:20 +0000

    fixed: problem where points in set shows up as 0 for an OutputSet that was deleted but still shows up in the history of some RecursiveOutputSet
    rebuilt jars

--- src/edu/bsu/julia/generators/DummyOutputSetGenerator.java
+++ src/edu/bsu/julia/generators/DummyOutputSetGenerator.java
@@ -3,14 +3,16 @@ package edu.bsu.julia.generators;
 import edu.bsu.julia.ComplexNumber;
 
 /**
- * An {@link OutputSetGenerator} that is a wrapper around an array of {@link ComplexNumber}
+ * An {@link OutputSetGenerator} that is a wrapper around an array of
+ * {@link ComplexNumber}
+ * 
  * @author Ben Dean
  */
 public class DummyOutputSetGenerator extends OutputSetGenerator {
 	private final ComplexNumber[] points;
-	
-	public DummyOutputSetGenerator(ComplexNumber[] p){
-		points = p;
+
+	public DummyOutputSetGenerator(ComplexNumber[] p) {
+		points = (p.length > 0) ? p : null;
 	}
 
 	public ComplexNumber[] doInBackground() {
