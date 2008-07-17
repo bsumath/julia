717a884c28de511af1996754b336ebb65e9fe514
benthedean <benthedean@localhost>
2008-07-17 14:51:24 +0000

    added: double click on new function is the same as clicking next
    rebuilt jars

--- src/edu/bsu/julia/gui/actions/AddFunctionAction.java
+++ src/edu/bsu/julia/gui/actions/AddFunctionAction.java
@@ -4,6 +4,8 @@ import java.awt.FlowLayout;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.AbstractAction;
 import javax.swing.ImageIcon;
@@ -24,19 +26,18 @@ import edu.bsu.julia.gui.QuadraticDialog;
 import edu.bsu.julia.gui.RealAffineLinearDialog;
 
 public class AddFunctionAction extends AbstractAction {
-	
+
 	private Julia parentFrame;
-	//for serializable interface: do not use
+	// for serializable interface: do not use
 	public static final long serialVersionUID = 0;
-	
+
 	public AddFunctionAction(Julia f) {
-		super("Add Function", new ImageIcon
-				(Thread.currentThread().getContextClassLoader().getResource
-						("addformula.png")));
+		super("Add Function", new ImageIcon(Thread.currentThread()
+				.getContextClassLoader().getResource("addformula.png")));
 		parentFrame = f;
 		putValue("SHORT_DESCRIPTION", "Add Function");
-		putValue("LONG_DESCRIPTION", "Add a new function to the present " +
-				"input function list.");
+		putValue("LONG_DESCRIPTION", "Add a new function to the present "
+				+ "input function list.");
 	}
 
 	public void actionPerformed(ActionEvent arg0) {
@@ -45,23 +46,36 @@ public class AddFunctionAction extends AbstractAction {
 		addFunctionDialog.setLocationRelativeTo(parentFrame);
 		addFunctionDialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 		addFunctionDialog.setLayout(new FlowLayout());
-		
+
 		JLabel choiceLabel = new JLabel("Please Choose a Function Type:");
 		addFunctionDialog.add(choiceLabel);
+
+		final JButton nextButton = new JButton("Next >>>");
 
 		String[] choices = new String[5];
 		choices[0] = "<html><h2>az + b</h2></html>";
 		choices[1] = "<html><h2>(az + b) / (cz + d)</h2></html>";
-		choices[2] = "<html><h2>az<sup>2</sup> + bz + c</h2></html>";	
+		choices[2] = "<html><h2>az<sup>2</sup> + bz + c</h2></html>";
 		choices[3] = "<html><h2>az<sup>3</sup> + b</h2></html>";
 		choices[4] = "<html><h2>[a, b ; c, d]z + [e ; f]</h2></html>";
 		final JList list = new JList(choices);
 		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		list.setVisibleRowCount(6);
 		list.setSelectedIndex(0);
+		list.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent ev) {
+				if (ev.getClickCount() == 2) {
+					for (ActionListener listener : nextButton
+							.getActionListeners()) {
+						listener.actionPerformed(null);
+					}
+				}
+			}
+		});
 		JScrollPane pane = new JScrollPane(list);
 		addFunctionDialog.add(pane);
-		
+
 		JPanel buttonPanel = new JPanel();
 		JButton cancelButton = new JButton("Cancel");
 		cancelButton.addActionListener(new ActionListener() {
@@ -71,14 +85,13 @@ public class AddFunctionAction extends AbstractAction {
 			}
 		});
 		buttonPanel.add(cancelButton);
-		
-		JButton nextButton = new JButton("Next >>>");
+
 		nextButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
 				int type = list.getSelectedIndex();
 				addFunctionDialog.setVisible(false);
 				addFunctionDialog.dispose();
-				switch(type) {
+				switch (type) {
 				case 0:
 					new LinearDialog(parentFrame, GUIUtil.NEW_DIALOG, null);
 					break;
@@ -92,17 +105,18 @@ public class AddFunctionAction extends AbstractAction {
 					new CubicDialog(parentFrame, GUIUtil.NEW_DIALOG, null);
 					break;
 				case 4:
-					new RealAffineLinearDialog(parentFrame, GUIUtil.NEW_DIALOG, null);
+					new RealAffineLinearDialog(parentFrame, GUIUtil.NEW_DIALOG,
+							null);
 				}
 			}
 		});
 		buttonPanel.add(nextButton);
 		addFunctionDialog.add(buttonPanel);
-		
-		addFunctionDialog.setSize(250,430);
+
+		addFunctionDialog.setSize(250, 430);
 		Point p = addFunctionDialog.getLocation();
-		p.x = p.x-125;
-		p.y = p.y-215;
+		p.x = p.x - 125;
+		p.y = p.y - 215;
 		addFunctionDialog.setLocation(p);
 		addFunctionDialog.setVisible(true);
 	}
