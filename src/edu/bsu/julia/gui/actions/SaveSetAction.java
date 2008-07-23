cc2d566a0c438f5837e9c50b93539005f719136f
benthedean <benthedean@localhost>
2008-07-23 16:41:02 +0000

    fixed: save output set now saves the .dat file

--- src/edu/bsu/julia/gui/actions/SaveSetAction.java
+++ src/edu/bsu/julia/gui/actions/SaveSetAction.java
@@ -2,55 +2,54 @@ package edu.bsu.julia.gui.actions;
 
 import java.awt.event.ActionEvent;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import javax.swing.AbstractAction;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileFilter;
 
 import edu.bsu.julia.Julia;
 import edu.bsu.julia.output.OutputSet;
 
 public class SaveSetAction extends AbstractAction {
 
-	private JSetFileFilter jsetFileFilter = new JSetFileFilter();
+	private DatFileFilter jsetFileFilter = new DatFileFilter();
 	private Julia parentFrame;
 	private File file = null;
-	// private DefaultListModel outputList;
-	// private int index;
+	private OutputSet set;
+
 	// for serializable interface: do not use
 	public static final long serialVersionUID = 0;
 
-	public SaveSetAction(Julia f, OutputSet func) {
+	private static final String FILE_EXTENSION = ".dat";
+
+	public SaveSetAction(Julia f, OutputSet s) {
 		super("Save Output Set");
 		putValue("SHORT_DESCRIPTION", "Save Output Set");
 		putValue("LONG_DESCRIPTION", "Save this output set to a file.");
+
 		parentFrame = f;
-		// outputList = listModel;
-		// index = i;
+		set = s;
 	}
 
 	public void actionPerformed(ActionEvent arg0) {
-		boolean status = false;
-		status = saveFile();
-		if (!status) {
-			JOptionPane.showMessageDialog(null, "IO error in saving file!!",
-					"File Save Error", JOptionPane.ERROR_MESSAGE);
-		}
-	}
-
-	public boolean saveFile() {
 		JFileChooser filechooser = new JFileChooser();
 		filechooser.setFileFilter(jsetFileFilter);
+
 		String path = parentFrame.getFilePath();
 		if (!path.equals(""))
 			filechooser.setCurrentDirectory(new File(path));
-		int result = filechooser.showSaveDialog(parentFrame);
-		if (result == JFileChooser.CANCEL_OPTION) {
-			return true;
-		} else if (result == JFileChooser.APPROVE_OPTION) {
+
+		switch (filechooser.showSaveDialog(parentFrame)) {
+		case JFileChooser.CANCEL_OPTION:
+			return;
+		case JFileChooser.APPROVE_OPTION:
 			file = filechooser.getSelectedFile();
-			if (!(file.getName().endsWith(".jset"))) {
-				file = new File(file.getAbsolutePath() + ".jset");
+			if (!(file.getName().endsWith(FILE_EXTENSION))) {
+				file = new File(file.getAbsolutePath() + FILE_EXTENSION);
 			}
 			if (file.exists()) {
 				int response = JOptionPane.showConfirmDialog(null,
@@ -58,24 +57,42 @@ public class SaveSetAction extends AbstractAction {
 						JOptionPane.OK_CANCEL_OPTION,
 						JOptionPane.QUESTION_MESSAGE);
 				if (response == JOptionPane.CANCEL_OPTION)
-					return false;
+					return;
 			}
 			parentFrame.setFilePath(file.getAbsolutePath());
-			return true;
-			// TODO fix this
-		} else {
-			return false;
+
+			File datFile = set.getFiles()[1];
+
+			try {
+				FileInputStream in = new FileInputStream(datFile);
+				FileOutputStream out = new FileOutputStream(file);
+				byte[] buf = new byte[1024];
+				int len = 0;
+				while ((len = in.read(buf)) != -1) {
+					out.write(buf, 0, len);
+				}
+				in.close();
+				out.close();
+				return;
+			} catch (IOException e) {
+				e.printStackTrace();
+				// since there is no break or return, this will show the error
+				// message for the default case
+			}
+		default:
+			JOptionPane.showMessageDialog(null, "IO error in saving file!!",
+					"File Save Error", JOptionPane.ERROR_MESSAGE);
 		}
 	}
 
-	private class JSetFileFilter extends javax.swing.filechooser.FileFilter {
+	private class DatFileFilter extends FileFilter {
 		public boolean accept(File file) {
-			return file.getName().toLowerCase().endsWith(".jset")
+			return file.getName().toLowerCase().endsWith(FILE_EXTENSION)
 					|| file.isDirectory();
 		}
 
 		public String getDescription() {
-			return "Julia Output Set (*.jset)";
+			return "Data File (*" + FILE_EXTENSION + ")";
 		}
 	}
 
