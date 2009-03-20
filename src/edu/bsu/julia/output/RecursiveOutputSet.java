874153eaf1ef7f654e6eeb113e3173bd83abf3fc
benthedean <benthedean@gmail.com>
2009-03-20 02:33:15 +0000

    fixed Help > About dialog updated the commons-math library changed the build.xml so it's quicker to load the jnlp from now on

--- src/edu/bsu/julia/output/RecursiveOutputSet.java
+++ src/edu/bsu/julia/output/RecursiveOutputSet.java
@@ -24,10 +24,6 @@ public class RecursiveOutputSet extends OutputSet {
 		outputSets = o;
 	}
 
-	public OutputSet[] getOutputSets() {
-		return outputSets;
-	}
-
 	public String toString() {
 		String s = "o" + getSubscript() + " = " + functionType.description()
 				+ " of ";
